96e6a59c87d2bf485e66e39cd1d48e15efacce2a
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2024-04-15 11:14:28 +0530

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import Todos from './features/todos/Todos';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/todos">Todos</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+import Home from './features/home/home';
 
 function App() {
   return (
-    <>
-      <Todos />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todos" element={<Todos />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/features/home/home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h2>This react app is created for learning purpose</h2>;
+}
+
+export default Home;
